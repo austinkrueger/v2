814f00cdf8bff1f96179eb8048ba191fa7878b05
Austin <akruegr@gmail.com>
2020-07-03 18:50:39 -0500

    Split skills into separate components

--- src/components/languages.js
+++ src/components/languages.js
@@ -9,13 +9,10 @@ function Languages() {
           <article
             className='flex-none w-full my-4 md:m-4 md:w-160 md:min-h-64 lg:w-160 lg:min-h-64'
             key={`language_item_${index}`}>
-            <div className='flex justify-center items-center rounded bg-white w-full h-full p-8 min-h-inherit md:group-hover:opacity-50 md:hover:scale-11/10x md:hover:opacity-important transition'>
+            <div className='flex justify-center items-center rounded bg-white w-full h-full p-8 min-h-inherit md:group-hover:opacity-50 md:hover:opacity-important transition'>
               <div className='flex justify-center'>
-                <img
-                  src={lang.icon}
-                  className='w-16 h-16 mr-16 fill-current mt-2'
-                />
-                <p>{lang.name}</p>
+                <img src={lang.icon} className='w-16 h-16 mr-16 mt-2' />
+                <p className='card-item-text'>{lang.name}</p>
               </div>
             </div>
           </article>
--- src/components/skills.js
+++ src/components/skills.js
@@ -4,7 +4,7 @@ import Frameworks from './frameworks';
 function Skills() {
   return (
     <div id='skills' className='md:pt-88 md:-mt-96'>
-      <h2 className='sticky top-0 z-40 pt-24 text-sm font-bold tracking-widest uppercase md:sr-only'>
+      <h2 className='top-0 z-40 pt-24 text-sm font-bold tracking-widest uppercase md:sr-only'>
         Skills
       </h2>
       <p className='text-xs tracking-widest uppercase mt-24 mb-16 p-16'>
